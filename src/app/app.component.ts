d675e3c35bb8c8414af399d76af1d6efbc472563
lmaos <luismanuelalfred.ottosantovenia@ruv.de>
2022-05-11 12:19:07 +0200

    added demo for new marker class

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,23 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
+import { MarkjsHighlight } from '@lmaos/ngx-markjs';
 
 @Component({
 	selector: 'app-root',
 	templateUrl: './app.component.html',
 	styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit {
+	@ViewChild('highlightCard') testCard!: ElementRef;
 	title = 'ngx-markjs-demo';
 	filteredText = 'Angular';
 	log(msg: string) {
 		console.log(msg);
+	}
+
+	ngAfterViewInit(): void {
+		MarkjsHighlight.mark(this.testCard.nativeElement, 'ngx');
+		MarkjsHighlight.markRange(this.testCard.nativeElement, [
+			{ start: 10, length: 5 },
+		]);
 	}
 }
